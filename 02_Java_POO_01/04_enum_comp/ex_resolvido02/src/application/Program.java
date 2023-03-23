e2028ba20b08535ac3926e16e1ecbcd6cd9afd41
Hebert do Nascimento <hebertn88@gmail.com>
2023-03-23 13:18:19 -0300

    resolucao exercicio resolvido 02 sem video

---
+++ 02_Java_POO_01/04_enum_comp/ex_resolvido02/src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import entities.Comment;
+import entities.Post;
+
+import java.time.Instant;
+
+public class Program {
+    public static void main(String[] args) {
+        //add Post
+        Instant moment = Instant.parse("2018-06-21T13:05:44-03:00");
+        String title = "Traveling to New Zealand";
+        String content = "I'm going to visit this wonderful country!";
+        int likes = 12;
+
+        Post post01 = new Post(moment, title, content, likes);
+        post01.addComment(new Comment("Have a nice trip"));
+        post01.addComment(new Comment("Wow that's awesome!"));
+
+        moment = Instant.parse("2018-07-28T23:14:19-03:00");
+        title = "Good night guys";
+        content = "See you tomorrow";
+        likes = 5;
+
+        Post post02 = new Post(moment, title, content, likes);
+        post02.addComment(new Comment("Good night"));
+        post02.addComment(new Comment("May the Force be with you"));
+
+        System.out.println(post01);
+        System.out.println(post02);
+    }
+}
---
+++ 02_Java_POO_01/04_enum_comp/ex_resolvido02/src/entities/Post.java
@@ -0,0 +1,74 @@
+package entities;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Post {
+    private Instant moment;
+    private String title;
+    private String content;
+    private Integer likes;
+
+    List<Comment> comments = new ArrayList<>();
+
+    public Post(){
+    }
+
+    public Post(Instant moment, String title, String content, Integer likes){
+        this.moment = moment;
+        this.title = title;
+        this.content = content;
+        this.likes = likes;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public void addComment(Comment comment) {
+        comments.add(comment);
+    }
+
+    @Override
+    public String toString() {
+        String comments = "";
+        for (Comment c : this.comments) {
+            comments += c.getText() + "\n";
+        }
+
+        return title + "\n" +
+        likes + " likes - " +
+        moment.atZone(ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")) + "\n" +
+        content + "\n" +
+        "Comments:\n" +
+        comments;
+
+    }
+}
